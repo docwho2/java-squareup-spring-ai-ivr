f72e21894cf8bce205537196b303d2acd6af4839
Steve Jensen <docwho2@gmail.com>
2025-11-27 05:36:20 -0600

    AWS Client Beans

--- SpringAI/src/main/java/cloud/cleo/squareup/AWSConfig.java
+++ SpringAI/src/main/java/cloud/cleo/squareup/AWSConfig.java
@@ -7,6 +7,7 @@ package cloud.cleo.squareup;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.http.SdkHttpClient;
 import software.amazon.awssdk.http.async.SdkAsyncHttpClient;
 import software.amazon.awssdk.http.crt.AwsCrtAsyncHttpClient;
@@ -14,11 +15,12 @@ import software.amazon.awssdk.http.crt.AwsCrtHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeAsyncClient;
 import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeClient;
-import software.amazon.awssdk.services.costexplorer.CostExplorerAsyncClient;
+import software.amazon.awssdk.services.costexplorer.CostExplorerClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
-import software.amazon.awssdk.services.pinpoint.PinpointAsyncClient;
-import software.amazon.awssdk.services.ses.SesAsyncClient;
-import software.amazon.awssdk.services.sns.SnsAsyncClient;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.pinpoint.PinpointClient;
+import software.amazon.awssdk.services.ses.SesClient;
+import software.amazon.awssdk.services.sns.SnsClient;
 
 /**
  * Build all needed AWS Clients used.
@@ -41,9 +43,6 @@ public class AWSConfig {
     @Bean(destroyMethod = "close")
     public SdkHttpClient crtSyncHttpClient() {
         return AwsCrtHttpClient.builder()
-                // tweak if you want:
-                // .maxConcurrency(512)
-                // .connectionTimeout(Duration.ofSeconds(1))
                 .build();
     }
 
@@ -60,11 +59,25 @@ public class AWSConfig {
                 .httpClient(crtAsyncHttpClient)
                 .build();
     }
+    
+    @Bean(destroyMethod = "close")
+    public DynamoDbClient dynamoDbClient(SdkHttpClient crtHttpClient) {
+        return DynamoDbClient.builder()
+                .httpClient(crtHttpClient)
+                .build();
+    }
 
     @Bean(destroyMethod = "close")
     public DynamoDbAsyncClient dynamoDbAsyncClient(SdkAsyncHttpClient crtAsyncHttpClient) {
         return DynamoDbAsyncClient.builder()
                 .httpClient(crtAsyncHttpClient)
+                .build();
+    }
+    
+    @Bean
+    public DynamoDbEnhancedClient dynamoDbEnhancedAsyncClient(DynamoDbClient dynamoDbClient) {
+        return DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(dynamoDbClient)
                 .build();
     }
 
@@ -76,34 +89,34 @@ public class AWSConfig {
     }
 
     @Bean(destroyMethod = "close")
-    public CostExplorerAsyncClient costExplorerAsyncClient(SdkAsyncHttpClient crtAsyncHttpClient) {
-        return CostExplorerAsyncClient.builder()
-                .httpClient(crtAsyncHttpClient)
+    public CostExplorerClient costExplorerAsyncClient(SdkHttpClient crtHttpClient) {
+        return CostExplorerClient.builder()
+                .httpClient(crtHttpClient)
                 .build();
     }
 
     @Bean(destroyMethod = "close")
-    public PinpointAsyncClient pinpointAsyncClient(SdkAsyncHttpClient crtAsyncHttpClient) {
-        return PinpointAsyncClient.builder()
-                .httpClient(crtAsyncHttpClient)
+    public PinpointClient pinpointAsyncClient(SdkHttpClient crtHttpClient) {
+        return PinpointClient.builder()
+                .httpClient(crtHttpClient)
                 .build();
     }
 
     @Bean(destroyMethod = "close")
-    public SesAsyncClient sesAsyncClient(SdkAsyncHttpClient crtAsyncHttpClient) {
-        return SesAsyncClient.builder()
-                .httpClient(crtAsyncHttpClient)
+    public SesClient sesAsyncClient(SdkHttpClient crtHttpClient) {
+        return SesClient.builder()
+                .httpClient(crtHttpClient)
                 .build();
     }
 
     @Bean(destroyMethod = "close")
-    public SnsAsyncClient snsAsyncClient(SdkAsyncHttpClient crtAsyncHttpClient) {
-        return SnsAsyncClient.builder()
+    public SnsClient snsAsyncClient(SdkHttpClient crtHttpClient) {
+        return SnsClient.builder()
                 // Force SMS sending to east because that's where all the 10DLC and campaign crap setup is done
                 // Otherwise have to pay for registrations and numbers in 2 regions, HUGE HASSLE (and more monthly cost)
                 // Also then all texts are sourced from the same phone number for consistancy
                 .region(Region.US_EAST_1)
-                .httpClient(crtAsyncHttpClient)
+                .httpClient(crtHttpClient)
                 .build();
     }
 
