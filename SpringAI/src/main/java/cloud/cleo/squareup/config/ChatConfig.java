a34e2964f00eee9c59345f9bed2d67da5bba3c5d
Steve Jensen <docwho2@gmail.com>
2025-12-09 06:48:29 -0600

    model tweaks

--- SpringAI/src/main/java/cloud/cleo/squareup/config/ChatConfig.java
+++ SpringAI/src/main/java/cloud/cleo/squareup/config/ChatConfig.java
@@ -110,6 +110,7 @@ public class ChatConfig {
                 .model(resolved)
                 .temperature(.1)
                 .topP(0.9)
+                .maxTokens(150)
                 .build();
     }
 
