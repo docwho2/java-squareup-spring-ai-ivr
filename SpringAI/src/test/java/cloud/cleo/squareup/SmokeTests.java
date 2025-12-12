ef772a5ce505bd3f01553c45ec6386fd98f67d6d
Steve Jensen <steve@cleo.cloud>
2025-12-11 19:27:16 -0600

    tweak weather test result

--- SpringAI/src/test/java/cloud/cleo/squareup/SmokeTests.java
+++ SpringAI/src/test/java/cloud/cleo/squareup/SmokeTests.java
@@ -109,7 +109,7 @@ public class SmokeTests extends AbstractLexAwsTestSupport {
         
         final var weather = getBotResponse(res);
         
-        boolean ok = weather.toLowerCase().matches("(?is).*?(°f|current temp).*");
+        boolean ok = weather.toLowerCase().matches("(?is).*?(°f|degrees|fahrenheit).*");
         log.info(ok ? "Weather Test Passed" : "Weather Test FAILED");
         assertTrue(ok, "Weaather test failed, response was: " + weather);
     }
